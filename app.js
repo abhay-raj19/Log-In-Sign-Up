1cab30699fe5c713fba04914b4bc17dbca0ea9fd
abhay-raj19 <rajpootabhay423@gmail.com>
2023-01-22 19:17:47 +0530

    Added the Passport authentication (cookies - method)

--- app.js
+++ app.js
@@ -4,30 +4,51 @@ const express =require("express");
 const bodyParser =require("body-parser");
 const ejs =require("ejs")
 
-const bcrypt = require('bcryptjs');
-const saltRounds = 10;
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const mongoose = require('mongoose');
 mongoose.set("strictQuery", false);
-mongoose.connect("mongodb://127.0.0.1:27017/userDB");
-//created a mongoose user schema
-const userSchema = new mongoose.Schema({
-    email : String,
-    password : String
-});
+
 
 
 
 //created a model for user by using schema
 
-const User = new mongoose.model("User",userSchema);
+
 
 const app = express();
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
-//home route
+
+app.use(session({
+    secret: "our little secret.",
+    resave:false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+mongoose.connect("mongodb://127.0.0.1:27017/userDB");
+
+
+const userSchema = new mongoose.Schema({
+    email : String,
+    password : String
+});
+
+userSchema.plugin(passportLocalMongoose);
+const User = new mongoose.model("User",userSchema);
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.get("/",function (req,res) {
     res.render("home");
 });
@@ -39,22 +60,43 @@ app.get("/login",function (req,res) {
 app.get("/register",function (req,res) {
     res.render("register");
 });
+
+app.get("/secrets",function (req,res) {
+    if (req.isAuthenticated()) {
+        res.render("secrets");  
+    } else {
+        res.redirect("/login");
+    }
+});
+
+
+app.get("/logout", function(req, res) {
+    req.logout(function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect('/'); 
+        }
+        
+    });
+    
+});
+
+
+
 //register route for posting 
 app.post("/register",function (req,res) {
 
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-        const newUser = new User({
-            email:req.body.username,
-            password:hash 
-        });
-        newUser.save(function (err) {
-            if(err){
-                console.log(err);
-            } else {
-                res.render("secrets");
-            }    
-        });
-    });
+    User.register({username: req.body.username},req.body.password,function (err,user) {
+        if (err) {
+            console.log(err);
+            res.redirect("/register");
+        } else{
+            passport.authenticate("local")(req,res,function () {
+                res.redirect("/secrets");
+            })
+        }
+    })
 
 
 
@@ -62,35 +104,22 @@ app.post("/register",function (req,res) {
 
 
 app.post("/login",function (req,res) {
-    const username = req.body.username;
-    const password = req.body.password;  
+   const user = new User({
+    username : req.body.username,
+    password : req.body.password
+   });
 
-    User.findOne({email:username},function (err,foundUser){
-        if (err) {
-            console.log(err);       
-        } else {
-            if (foundUser) {
-                bcrypt.compare(password, foundUser.password, function(err, result) {
-                    if (result === true) {
-                        res.render("secrets");
-                    }
-                });
-                
-                
-            }
-        }
-        
-    });
+   req.login(user,function (err){
+    if(err){
+        console.log(err);
+    } else {
+        passport.authenticate("local")(req,res,function () {
+            res.redirect("/secrets");
+        });
+    }
+   });
 
 }); 
-
-
-
-
-
-
-
-
 
 app.listen(process.env.PORT || 3000 , function () {
     console.log("Server Started At Port 3000");
